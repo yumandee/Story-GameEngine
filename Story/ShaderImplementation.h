17240f114ceb19a981754e04783a9acf15da73a6
yumandee <yumandee@yahoo.com>
2021-12-21 03:28:36 -0500

    remove glfw as submodule

--- Story/ShaderImplementation.h
+++ Story/ShaderImplementation.h
@@ -10,6 +10,6 @@ namespace Story
       virtual void Load(const std::string &vertexFile, const std::string &fragmentFile) = 0;
       virtual void SetVec2IntUniform(const std::string &unifName, int first, int second) = 0;
       virtual void Use() = 0;
-      // virtual ~ShaderImplementation(){};
+      virtual ~ShaderImplementation(){};
    };
 }
